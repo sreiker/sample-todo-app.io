c858d1e1bb8af9a82fc4d18bc2d820d1a5f9ab40
Sunil Rajput <48656178+sreiker@users.noreply.github.com>
2024-03-07 19:15:58 +0530

    Update test_sample_todo_app.py

--- test_sample_todo_app.py
+++ test_sample_todo_app.py
@@ -17,7 +17,7 @@ driver = webdriver.Remote(
     desired_capabilities= capabilities,
     command_executor= url
 )
-driver.get("http://localhost:8081/")
+driver.get("https://sreiker.github.io/sample-todo-app.io/")
 driver.find_element_by_name("li3").click()
 
 textbox = driver.find_element_by_id("sampletodotext")
